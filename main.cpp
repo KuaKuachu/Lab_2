1f0d8efa207f7582db2d2a3f1aa001ee5fafbabd
Fedorova <kuakuachu@yandex.ru>
2015-09-18 11:08:35 +0400

    пункт 1.5

--- main.cpp
+++ main.cpp
@@ -2,9 +2,12 @@
 
 int main()
 {
-   int x,y;
+   int x,y, sum;
    cout <<"Vvedite chislo x:\n";
    cin>> x;
    cout <<"Vvedite chislo y:\n";
    cin>> y;
+   sum=x+y;
+   cout <<"Sum: ";
+   cout <<sum;
 }
