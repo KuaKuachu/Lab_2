7e8b32d20f235e188d96cb85b4d65ab5c4ed91c0
Fedorova <kuakuachu@yandex.ru>
2015-09-18 12:41:43 +0400

    пункт 3.3

--- main.cpp
+++ main.cpp
@@ -2,16 +2,16 @@
 //программа является учебной
 int main()
 {
-   int x,y, sum,ruz;
+   int x,y, sum,ruz,del;
    cout <<"Vvedite chislo x:\n";
    cin>> x;
    cout <<"Vvedite chislo y:\n";
    cin>> y;
    sum=x+y;
    ruz=x-y;
-   cout <<"Sum: ";
-   cout <<sum;
-   cout << "\n";
-   cout <<"Ruz: ";
-   cout <<ruz;
+   del=x/y;
+   cout <<"Sum: " <<sum <<'\n'
+        <<"Ruz: " <<ruz <<'\n'
+        <<"Del: " <<del <<'\n';
+
 }
